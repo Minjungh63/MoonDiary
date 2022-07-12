47b9c9f7fccd2a3be622e4de287b0726b194a759
mangobam <2835223@ewhain.net>
2022-07-12 14:35:48 +0900

    [feature] add diaryView

--- client/diary/views.py
+++ client/diary/views.py
@@ -1,3 +1,4 @@
+from urllib import response
 from django.views.decorators.csrf import csrf_exempt
 import json
 from django.views import View
@@ -7,23 +8,32 @@ from .models import Diary, AI
 
 class mainView(View):
     def get(self, request):
-        id = json.loads(request.body.userId)
+        id = request.GET['userId']
         print(id)
         sdata = AI.objects.select_related('diaryId').get(diaryId=id)
 
         print(sdata)
         return JsonResponse(sdata, status=200)
     
-class diaryView(View):
+class writeView(View):
     def post(self, request):
-        data = json.loads(request.body)
-        id = data['userId'].replace('"','')
+        data = Diary()
+        data.contents = request.POST['contents']
+        data.weather = request.POST['weather']
+        data.userId = request.POST['userId']
+        data.title = request.POST['title']
+        data.save()
+        return HttpResponse(status=201)
+    
+class checkView(View):
+    def get(self, request):#일단 diary 테이블 데이터만 넘겨줌
+        dId = request.GET['diaryId']
+        data = Diary.objects.get(diaryId=dId)
+        return JsonResponse(data,response=200)
         
-        date = data['date'].replace('"','')
-        weather = data['weather'].replace('"','')
-        title = data['title'].replace('"','')
-        contents = data['userId'].replace('"','')
-
-        
-        print(sdata)
-        return JsonResponse(sdata, status=200)
+    def post(self, request):
+        dId = request.GET['diaryId']
+        data = Diary.objects.get(diaryId=dId)
+        data.liked = 1
+        data.save()
+        return HttpResponse(status=201)
