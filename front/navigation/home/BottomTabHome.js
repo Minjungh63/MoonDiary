8c4f5d2096de603eda900bfc8ec755220be61f84
MinJung <52921222+Minjungh63@users.noreply.github.com>
2022-07-14 02:09:10 +0900

    [add] 설정 페이지 구성 완료 & 통계 계산 구현

--- front/navigation/home/BottomTabHome.js
+++ front/navigation/home/BottomTabHome.js
@@ -1,8 +1,8 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import StackHome from './StackHome';
-import StackStatistics from '../statistics/StackStatistics';
+import StatisticsView from '../../pages/StatisticsView';
 import StackFavorites from '../favorite/StackFavorites';
-import StackSettings from '../setting/StackSettings';
+import SettingsView from '../../pages/SettingsView';
 
 export default function BottomTabHome() {
   const Tab = createBottomTabNavigator();
@@ -10,8 +10,8 @@ export default function BottomTabHome() {
     <Tab.Navigator initialRouteName="StackHome" screenOptions={{ headerShown: false }}>
       <Tab.Screen name="StackHome" component={StackHome} />
       <Tab.Screen name="StackFavorites" component={StackFavorites} />
-      <Tab.Screen name="StackStatistics" component={StackStatistics} />
-      <Tab.Screen name="StackSettings" component={StackSettings} />
+      <Tab.Screen name="StatisticsView" component={StatisticsView} />
+      <Tab.Screen name="SettingsView" component={SettingsView} />
       {/* 다음페이지가 없으므로 바로 View로 전달해도 됨 */}
     </Tab.Navigator>
   );
