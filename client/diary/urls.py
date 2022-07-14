2ee95af57e0a3eb7dc302168d0e87ec729c5f20d
mangobam <2835223@ewhain.net>
2022-07-14 16:47:21 +0900

    [chore] fix url

--- client/diary/urls.py
+++ client/diary/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import checkView, mainView, writeView, chooseView, likeView
+from .views import checkView, mainView, writeView, chooseView, likeView, resultView
   
 urlpatterns = [
     path('', mainView.as_view()),
     path('write', writeView.as_view()),
     path('<int:diaryId>', checkView.as_view()),
     path('write/mood', chooseView.as_view()),
-    path('like', likeView.as_view())
+    path('like/<int:diaryId>', likeView.as_view()),
+    path('write/result', resultView.as_view())
 ]
