5b016939b4034cef6d0c11ac494ba61c29e41d2c
mangobam <2835223@ewhain.net>
2022-07-14 15:36:45 +0900

    [add] add url

--- client/diary/urls.py
+++ client/diary/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import checkView, mainView, writeView, chooseView
+from .views import checkView, mainView, writeView, chooseView, likeView
   
 urlpatterns = [
     path('', mainView.as_view()),
     path('write', writeView.as_view()),
     path('<int:diaryId>', checkView.as_view()),
-    path('write/mood', chooseView.as_view())
+    path('write/mood', chooseView.as_view()),
+    path('diary/like', likeView.as_view())
 ]
