199309bcb3fc0f94397b8ac345772c4b90daee4c
MinJung <52921222+Minjungh63@users.noreply.github.com>
2022-07-15 19:12:48 +0900

    [add] 통계페이지 데이터 전달 구현

--- client/config/settings.py
+++ client/config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'users',
     'diary',
     'AI',
+    'statistic',
 ]
 
 MIDDLEWARE = [
--- client/statistic/views.py
+++ client/statistic/views.py
@@ -1,36 +1,24 @@
 from datetime import  datetime
 from django.views import View
-from django.core import serializers
-from rest_framework.response import Response
+from django.http import JsonResponse
 from diary.models import Diary
 from AI.models import AI
 from django.db.models import Count
+import json
+
 # Create your views here.
 class StatisticsView(View):
     def get(self, request):
         userId = request.GET['userId']
-        # emotionRank = Diary.objects.raw(f"SELECT emotion as id, count(*) as day FROM DIARY INNER JOIN AI using(diaryId) where strftime('%Y-%m',date)=strftime('%Y-%m', datetime('now')) AND userId = 'test' group by emotion")
         month = Diary.objects.filter(date__contains=datetime.now().strftime("%Y-%m")).filter(userId=userId)
-        emotion = AI.objects.filter(diaryId__in=month.values('diaryId')).values('emotion').annotate(day=Count('emotion'))
-        # data = serializers.serialize("json", emotionRankList)
-        return Response(data =emotion, status=200)
-
-# class Statistics:
-#     diaryid=''
-#     year=''
-#     month=''
-#     emotion=''
-#     def __init__(self, diaryId, year, month, emotion):
-#         self.diaryId = diaryId
-#         self.year = year
-#         self.month = month
-#         self.emotion = emotion
-    
-
-# statistics = []
-# diaryId = data.diaryId
-# year = str(data.date).split("-")[0]
-# month = str(data.date).split("-")[1]
-# emotion = data.emotion
-# statistics = Statistics(diaryId=diaryId, year=year, month=month, emotion=emotion)
-# statisticsList.append(statistics)
+        emotion = AI.objects.filter(diaryId__in=month.values_list('diaryId')).values('emotion').annotate(day=Count('emotion'))
+        res = []
+        for i in range(len(emotion)):
+            temp = {
+                "emotion": emotion[i]['emotion'],
+                "day": emotion[i]['day']
+            }
+            res.append(temp)
+        jsonObj = json.dumps(res, default=str)
+        sdata = json.loads(jsonObj)
+        return JsonResponse(sdata, status=200, safe=False)
