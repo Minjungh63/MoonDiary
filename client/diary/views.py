76c5c589cf37f663e6bf816dc0efe4133aea95bc
Heewon Lee <heewonlee@ewhain.net>
2022-07-18 18:08:41 +0900

    [chore] ai.py 사용 예시

--- client/diary/views.py
+++ client/diary/views.py
@@ -6,15 +6,18 @@ from users.models import User
 from AI.models import AI
 from diary.models import Diary
 from django.core import serializers
+from AI import ai
 
 # Create your views here.
 
+
 class mainView(View):
     def post(self, request):
-        data = json.loads(request.body)        
+        data = json.loads(request.body)
         id = data['userId']
-        
-        data = AI.objects.select_related('diaryId').values_list('diaryId','emotion','diaryId__date').filter(diaryId__userId=id)
+
+        data = AI.objects.select_related('diaryId').values_list(
+            'diaryId', 'emotion', 'diaryId__date').filter(diaryId__userId=id)
         res = []
         for i in range(len(data)):
             temp = {
@@ -23,24 +26,29 @@ class mainView(View):
                 "date": data[i][2]
             }
             res.append(temp)
-            
+
         jsonObj = json.dumps(res, default=str)
         sdata = json.loads(jsonObj)
         return JsonResponse(sdata, status=200, safe=False)
-    
+
+
 class writeView(View):
     def post(self, request):
         temp = json.loads(request.body)
-        Diary.objects.create(userId=User.objects.get(userId="test"), contents=temp['contents'], weather=temp['weather'], title=temp['title'])       
+        Diary.objects.create(userId=User.objects.get(
+            userId="test"), contents=temp['contents'], weather=temp['weather'], title=temp['title'])
         did = Diary.objects.filter(userId="test").last()
-
+        doc = temp['contents']
+        emotion = ai.run_emotion(doc)
         sdata = {
             "diaryId": did.diaryId
         }
+
         return JsonResponse(sdata, status=201)
-    
-class checkView(View):#일기 확인 페이지
-    def get(self, diaryId):#일단 diary 테이블 데이터만 넘겨줌
+
+
+class checkView(View):  # 일기 확인 페이지
+    def get(self, diaryId):  # 일단 diary 테이블 데이터만 넘겨줌
         data = Diary.objects.get(diaryId=diaryId)
         sdata = {
             "diaryId": data.diaryId,
@@ -51,9 +59,11 @@ class checkView(View):#일기 확인 페이지
             "liked": data.liked
         }
         return JsonResponse(sdata, status=200)
+
     def put(self, request):
         return JsonResponse()
-    
+
+
 class moodView(View):
     def get(self, request, diaryId):
         emotion = AI.objects.get(diaryId=diaryId)
@@ -61,7 +71,7 @@ class moodView(View):
             "emotions": emotion.emotion
         }
         return JsonResponse(sdata, status=200)
-    
+
     def post(self, request):
         data = json.loads(request.body)
         dId = data['diaryId']
@@ -70,11 +80,13 @@ class moodView(View):
         newemo.emotion = semo
         newemo.save()
         return HttpResponse(status=201)
-    
-class likeView(View):#즐겨찾기 페이지
+
+
+class likeView(View):  # 즐겨찾기 페이지
     def get(self, request):
         id = request.GET['userId']
-        data = AI.objects.select_related('diaryId').values_list('diaryId','emotion','comment','diaryId__date','diaryId__weather','diaryId__title').filter(diaryId__userId=id, diaryId__liked=1)
+        data = AI.objects.select_related('diaryId').values_list(
+            'diaryId', 'emotion', 'comment', 'diaryId__date', 'diaryId__weather', 'diaryId__title').filter(diaryId__userId=id, diaryId__liked=1)
         res = []
         for i in range(len(data)):
             temp = {
@@ -86,26 +98,27 @@ class likeView(View):#즐겨찾기 페이지
                 "title": data[i][5],
             }
             res.append(temp)
-            
+
         jsonObj = json.dumps(res, default=str)
         sdata = json.loads(jsonObj)
         return JsonResponse(sdata, status=200, safe=False)
-    
+
     def post(self, request):
-        data = json.loads(request.body)        
+        data = json.loads(request.body)
         dId = data['diaryId']
         dlike = data['liked']
         adata = Diary.objects.get(diaryId=dId)
         adata.liked = dlike
         adata.save()
         return JsonResponse({"message": "update success"}, status=201)
-    
+
+
 class resultView(View):
     def get(self, request):
         dId = request.GET['diaryId']
         if AI.objects.only('image').get(diaryId=dId) == NULL:
             return JsonResponse({"message: not yet"}, status=200)
-        
+
         else:
             data = AI.objects.get(diaryId=dId)
             jsonObj = json.dumps(data)
