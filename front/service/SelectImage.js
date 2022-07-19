bef3042a90011c28d8f9d5d5bc51a90bbd0e0250
Im_younique <48540492+DongYounYim@users.noreply.github.com>
2022-07-19 14:27:15 +0900

    [feat] 감정, 날씨에 따른 이미지 불러오는 service 추가

---
+++ front/service/SelectImage.js
@@ -0,0 +1,26 @@
+const emotion_list = {
+  joy: require('../assets/img/emotion/joy.png'),
+  love: require('../assets/img/emotion/love.png'),
+  angry: require('../assets/img/emotion/angry.png'),
+  sad: require('../assets/img/emotion/sad.png'),
+  surprised: require('../assets/img/emotion/surprised.png'),
+  tired: require('../assets/img/emotion/tired.png'),
+  neutral: require('../assets/img/emotion/neutral.png'),
+  fear: require('../assets/img/emotion/fear.png'),
+};
+
+const weather_list = {
+  cloudy: require('../assets/img/weather/cloudy.png'),
+  hot: require('../assets/img/weather/hot.png'),
+  rainy: require('../asset/img/weather/rainy.png'),
+  stormy: require('../assets/img/weather/stormy.png'),
+  sunny: require('../assets/img/weather/sunny.png'),
+};
+
+export const getEmtionRequire = (emotion) => {
+  return emotion_list[emotion];
+};
+
+export const getWeatherRequire = (weather) => {
+  return weather_list[weather];
+};
