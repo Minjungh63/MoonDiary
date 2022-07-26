4ff8b0a02eabc3e2ae267c5ea7d7ea729a3642df
rladbrua0207 <rladbrua0207@gmail.com>
2022-07-26 16:22:50 +0900

    [fix] url 변경

--- client/diary/urls.py
+++ client/diary/urls.py
@@ -3,6 +3,6 @@ from .views import mainView, writeView, moodView, likeView
 urlpatterns = [
     path('', mainView.as_view()),
     path('write', writeView.as_view()),
-    path('mood/', moodView.as_view()),
-    path('like/', likeView.as_view()),
+    path('mood', moodView.as_view()),
+    path('like', likeView.as_view()),
 ]
