07faa897db63a5208742b4a67e3c165dc2eb995a
yugyeom <rladbrua0207@gmail.com>
2022-07-27 11:30:03 +0000

    [chore] AI task 코드수정

--- client/AI/tasks.py
+++ client/AI/tasks.py
@@ -29,10 +29,6 @@ def run_pixray(doc, dId):
     keyW = keyword_extract(doc)
     keyW = keyW.replace(' ', '_')
     print(keyW)
-    os.chdir("/home/lab/yugyeom/lab/MoonDiary/client/AI/drawing_diary/pixray")
-    sys.path.append("/home/lab/yugyeom/lab/MoonDiary/client/AI/drawing_diary/pixray")
-    subprocess.run(
-        ["python", "pixray.py", "--drawer=line_sketch", "--prompt=%s" % (keyW), "--outdir=../output"])
     image_path = 'http://168.188.123.158:8000/media/output.png' #동적으로 변경해야함
 
     data = AI.objects.get(diaryId=dId)
